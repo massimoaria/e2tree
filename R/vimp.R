c48ad6fd1d88f02b7ad6578cfd01ecd117882a32
massimoaria <aria@unina.it>
2023-05-26 11:58:36 +0200

    minor fix

--- R/vimp.R
+++ R/vimp.R
@@ -61,38 +61,38 @@ vimp <- function(tree, response, X){
 
   names(vimp_prob)[-1] <- paste("AccDec_",names(vimp_prob)[-1])
 
-vimp <- tree %>%
-  mutate(Nimp = .data$decImp*.data$n/.data$n[1],
-         Pimp = .data$decProb*.data$n/.data$n[1]) %>%
-  group_by(.data$variable) %>%
-  summarize(vimp = sum(.data$Nimp, na.rm=TRUE),
-            pimp = sum(.data$Pimp, na.rm=TRUE)) %>%
-  drop_na(.data$variable) %>%
-  # mutate(vimpNorm = .data$vimp/sum(.data$vimp)*100,
-  #        pimpNorm = .data$pimp/sum(.data$pimp)*100) %>%
-  arrange(desc(.data$vimp), by_group=FALSE) %>%
-  left_join(vimp_resp, by = "variable") %>%
-  left_join(vimp_prob, by = "variable")
+  vimp <- tree %>%
+    mutate(Nimp = .data$decImp*.data$n/.data$n[1],
+           Pimp = .data$decProb*.data$n/.data$n[1]) %>%
+    group_by(.data$variable) %>%
+    summarize(vimp = sum(.data$Nimp, na.rm=TRUE),
+              pimp = sum(.data$Pimp, na.rm=TRUE)) %>%
+    drop_na(.data$variable) %>%
+    # mutate(vimpNorm = .data$vimp/sum(.data$vimp)*100,
+    #        pimpNorm = .data$pimp/sum(.data$pimp)*100) %>%
+    arrange(desc(.data$vimp), by_group=FALSE) %>%
+    left_join(vimp_resp, by = "variable") %>%
+    left_join(vimp_prob, by = "variable")
 
-names(vimp)[1:3] <- c("Variable","MeanImpurityDecrease","MeanAccuracyDecrease")
+  names(vimp)[1:3] <- c("Variable","MeanImpurityDecrease","MeanAccuracyDecrease")
 
-# Minimal theme + blue fill color
-if (isTRUE(graph)){
-  pImp <-ggplot(vimp, aes(y=.data$Variable, x=.data$MeanImpurityDecrease)) +
-    geom_bar(stat="identity", fill="steelblue") +
-    scale_y_discrete(limits = rev(vimp$Variable))+
-    labs(title="Variable Importance Plot", x = "Mean Impurity Decrease", y = "Variance") +
-    theme_minimal()
+  # Minimal theme + blue fill color
+  if (isTRUE(graph)){
+    pImp <-ggplot(vimp, aes(y=.data$Variable, x=.data$MeanImpurityDecrease)) +
+      geom_bar(stat="identity", fill="steelblue") +
+      scale_y_discrete(limits = rev(vimp$Variable))+
+      labs(title="Variable Importance Plot", x = "Mean Impurity Decrease", y = "Variance") +
+      theme_minimal()
 
-  pAcc <-vimp %>%
-    arrange(desc(.data$MeanAccuracyDecrease), by_group=FALSE)
-  pAcc <- pAcc %>%
-    ggplot(aes(y=.data$Variable, x=.data$MeanAccuracyDecrease)) +
-    geom_bar(stat="identity", fill="steelblue") +
-    scale_y_discrete(limits = rev(pAcc$Variable))+
-    labs(title="Variable Importance Plot", x = "Mean Accuracy Decrease", y = "Variance") +
-    theme_minimal()
-}
-res <- list(vimp=vimp, g_imp=pImp, g_acc=pAcc)
-return(res)
+    pAcc <-vimp %>%
+      arrange(desc(.data$MeanAccuracyDecrease), by_group=FALSE)
+    pAcc <- pAcc %>%
+      ggplot(aes(y=.data$Variable, x=.data$MeanAccuracyDecrease)) +
+      geom_bar(stat="identity", fill="steelblue") +
+      scale_y_discrete(limits = rev(pAcc$Variable))+
+      labs(title="Variable Importance Plot", x = "Mean Accuracy Decrease", y = "Variance") +
+      theme_minimal()
+  }
+  res <- list(vimp=vimp, g_imp=pImp, g_acc=pAcc)
+  return(res)
 }
