e705774dea75f3f4f25e40294d11c72713ff2344
Agostino Gnasso <agostinognasso@MacAgos.fritz.box>
2023-05-29 11:39:35 +0200

    prova

--- R/e2tree.R
+++ R/e2tree.R
@@ -8,7 +8,7 @@ utils::globalVariables(c("node", "Y", "p", "variable", "decImp", "splitLabel", "
 #' @param data is a data frame in which to interpret the variables named in the formula.
 #' @param D is the dissimilarity matrix.
 #' @param setting is a list containing the setting parameters for tree building procedure.
-#' @param method is a character...
+#' @param method is a character method...
 #' Default is \code{setting=list(impTotal=0.1, maxDec=0.01, n=5, level=5, tMax=5)}.
 #'
 #' @return a e2tree object.
