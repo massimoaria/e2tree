2309cf6911d0f432d82a0e9192e0bce522c022de
Agostino Gnasso <agostinognasso@MacAgos.local>
2025-02-12 09:38:31 +0100

    Add new function and upgrade

---
+++ R/createDisMatrix_new.R
@@ -0,0 +1,195 @@
+utils::globalVariables(c("resp", "W")) # to avoid CRAN check errors for tidyverse programming
+#' Dissimilarity matrix
+#'
+#' The function createDisMatrix creates a dissimilarity matrix among observations from an ensemble tree.
+#'
+#' @param ensemble is an ensemble tree object (for the moment ensemble works only with random forest objects)
+#' @param data a data frame containing the variables in the model. It is the data frame used for ensemble learning.
+#' @param label is a character. It indicates the response label.
+#' @param parallel to speed up your code. Run all the process in parallel on your laptop using your cores-1
+#'
+#' @return A dissimilarity matrix. This is a dissimilarity matrix measuring the discordance between two observations concerning a given classifier of a random forest model.
+#'
+#' @examples
+#' \donttest{
+#' ## Classification
+#' data("iris")
+#'
+#' # Create training and validation set:
+#' smp_size <- floor(0.75 * nrow(iris))
+#' train_ind <- sample(seq_len(nrow(iris)), size = smp_size)
+#' training <- iris[train_ind, ]
+#' validation <- iris[-train_ind, ]
+#' response_training <- training[,5]
+#' response_validation <- validation[,5]
+#'
+#' # Perform training:
+#' ensemble <- randomForest::randomForest(Species ~ ., data=training,
+#' importance=TRUE, proximity=TRUE)
+#' 
+#' D <- createDisMatrix(ensemble, data=training, label = "Species", parallel = FALSE)
+#'
+#'
+#' ## Regression
+#' data("mtcars")
+#' 
+#' # Create training and validation set:
+#' smp_size <- floor(0.75 * nrow(mtcars))
+#' train_ind <- sample(seq_len(nrow(mtcars)), size = smp_size)
+#' training <- mtcars[train_ind, ]
+#' validation <- mtcars[-train_ind, ]
+#' response_training <- training[,1]
+#' response_validation <- validation[,1]
+#' 
+#' # Perform training
+#' ensemble = randomForest::randomForest(mpg ~ ., data=training, ntree=1000, 
+#' importance=TRUE, proximity=TRUE)
+#' 
+#' D = createDisMatrix(ensemble, data=training, label = "mpg", parallel = FALSE)  
+#' 
+#' }
+#' @export
+
+createDisMatrix_new <- function(ensemble, data, label, parallel = FALSE) {
+  row.names(data) <- NULL
+  
+  # Determine the type of the ensemble (e.g, regression or classification)
+  type <- ensemble$type
+  
+  
+  # Predict nodes or leaf indices based on the ensemble type
+  switch(class(ensemble)[length(class(ensemble))],
+         randomForest = {
+           # If the ensemble is a random forest, get the terminal nodes for each tree
+           obs <- as.data.frame(attr(predict(ensemble, newdata = data, nodes = TRUE), "nodes"))
+           n_tree <- ensemble$ntree
+         },
+         xgb.Booster = 
+           {
+             # If the ensemble is an xgboost model, get the leaf indices for each tree
+             obs <- as.data.frame(predict(ensemble, newdata = data_XGB, predleaf = TRUE))
+             # Remove columns with all zeros
+             obs <- obs[, colSums(obs) != 0L] #PROBABILMENTE NON SERVE, PERCHE NON HO PIU IL NUMERO MAX DI ALBERI PRODOTTI
+             n_tree <- ensemble$niter
+           })
+  
+  # Ensure data is a data.frame and the response is a factor
+  class(data) <- "data.frame"
+  if (!inherits(data[[label]], "factor")) {
+    # Convert the response variable to a factor if it is not already
+    data[[label]] <- factor(data[[label]])
+  }
+  
+  
+  
+  # Add observation IDs and tree indices to the observations
+  obs <- cbind(row.names(obs), obs)
+  names(obs) <- c("OBS", paste("Tree", seq(1, (ncol(obs) - 1L)), sep = ""))
+  row.names(obs) <- NULL
+  
+  nodes <- sort(unique(as.numeric(as.matrix(((obs %>%
+                                                select(starts_with("Tree"))))))))
+  
+  
+  
+  # Add the response variable to the observations
+  if (type == "classification") {
+    # For classification, retain the factor response
+    obs$resp <- data[as.numeric(obs$OBS), label]
+  } else {
+    # For regression, convert the response to numeric
+    obs$resp <- as.numeric(as.character(data[obs$OBS, label]))
+  }
+  
+  # Number of trees in the ensemble
+  ntree <- ncol(obs) - 2L
+  
+  # Initialize matrices
+  w <- matrix(NA, nrow(obs), ntree)  # Matrix to store weights
+  a <- Matrix(0L, nrow(obs), nrow(obs), sparse = TRUE)  # Sparse matrix for co-occurrences
+  
+  # Register parallel backend
+  if (parallel == TRUE) {
+    # If parallel processing is enabled, use all minus one core
+    no_cores <- detectCores() - 1L
+    registerDoParallel(cores = no_cores)
+  } else {
+    # If not using parallel processing, use one core
+    no_cores <- detectCores() - (max(detectCores()) - 1)  
+    registerDoParallel(cores = no_cores)
+  }
+  
+  
+  ## Start the computation
+  if (type == "classification") {
+    cat("Classification Framework\n")
+    
+    # Progress bar setup
+    # pb <- txtProgressBar(min = 0L, max = ensemble$ntree, style = 3L)
+    pb <- txtProgressBar(min = 0L, max = n_tree, style = 3L)
+    
+    # Parallel computation
+    #results <- foreach(i = seq_len(ensemble$ntree), .packages = c('dplyr', 'Matrix')) %dopar% {
+    results <- foreach(i = seq_len(ntree), .packages = c("Rcpp")) %dopar% {
+      compute_cooccurrences_cpp(type, obs, w, i)
+    }
+    
+    # Update progress bar and combine results
+    for (i in seq_along(results)) {
+      setTxtProgressBar(pb, i)
+      a <- as.matrix(a + results[[i]])
+    }
+    close(pb)
+    stopImplicitCluster()
+    
+  } else {
+    cat("Regression Framework\n")
+    #maxvar <- variance(obs$resp)
+    #maxvar <- diff(range(obs$resp))^2L / 9L
+    #maxvar <- var(obs$resp)*(nrow(obs)-1)/nrow(obs) # population variance 
+    
+    
+    # Progress bar setup
+    # pb <- txtProgressBar(min = 0L, max = ensemble$ntree, style = 3L)
+    pb <- txtProgressBar(min = 0L, max = n_tree, style = 3L)
+    
+    # Parallel computation
+    #results <- foreach(i = seq_len(ensemble$ntree), .packages = c('dplyr', 'Matrix')) %dopar% {
+    results <- foreach(i = seq_len(ntree), .packages = c("Rcpp")) %dopar% {
+      compute_cooccurrences_cpp(type, obs, w, i, maxvar = diff(range(obs$resp))^2 / 9L)
+    }
+    
+    # Update progress bar and combine results
+    for (i in seq_along(results)) {
+      setTxtProgressBar(pb, i)
+      a <- as.matrix(a + results[[i]])
+    }
+    close(pb)
+    stopImplicitCluster()
+  }
+  
+  # Similarity matrix
+  ## a is the similarity matrix
+  ## aa is the maximum similarity matrix
+  aa <- diag(a)
+  aa <- outer(aa, aa, "maxValue")
+  a <- a / aa  # now a is scaled between 0 and 1
+  
+  # Dissimilarity matrix among observations (respect to the co-occurrences in the same node)
+  dis <- 1L - a
+  row.names(dis) <- colnames(dis) <- obs$OBS
+  #dis <- as.matrix(dis)
+  
+  return(dis)
+}
+
+
+## Variance
+variance <- function(x){
+  sum((x-mean(x))^2)/length(x)
+}
+
+
+maxValue <- function(x,y){
+  apply(cbind(x,y),1L,max)
+}
--- R/vimp.R
+++ R/vimp.R
@@ -7,6 +7,7 @@ utils::globalVariables(c("decImp", "n", "pred", "variable", "Nimp", "vimp", "dec
 #'
 #' @param fit is a e2tree object
 #' @param data is a data frame in which to interpret the variables named in the formula.
+#' @param type Specify the type. The default is ‘classification’, otherwise ‘regression’.
 #'
 #' @return a data frame containing variable importance metrics.
 #'
@@ -16,77 +17,105 @@ utils::globalVariables(c("decImp", "n", "pred", "variable", "Nimp", "vimp", "dec
 #'
 #' @export
 #'
-vimp <- function(fit, data){
-
+vimp <- function(fit, data, type = "classification") {
   data <- as.data.frame(data)
   row.names(data) <- NULL
   tree <- fit$tree
-  variables <- names(attr(fit$terms, "dataClasses"))[1]
-  response <- data[,variables]
-  ## Calcolo decrease of Accuracy ##
-  row.names(tree) <- tree$node
-  tree$prob <- as.numeric(tree$prob)
-  t <- row.names(tree)[tree$terminal==FALSE]
-  tL <- as.character(tree[t,"node"]*2)
-  tR <- as.character(tree[t,"node"]*2+1)
-  tree[t,"probChildren"] <- (tree[tL,"prob"]*tree[tL,"n"]/tree[t,"n"]) + (tree[tR,"prob"]*tree[tR,"n"]/tree[t,"n"])
-  #attach(X)
-  names(response) <- row.names(data)
-  for (i in t){
-    obs <- row.names(data)[unlist(tree[i,"obs"])]
-    path <- tree[i,"splitLabelSur"]
-    x <- data[obs,]
-    indL <- obs[eval(parse(text=paste("x$",path)))]
-    indR <- setdiff(obs,indL)
-    probL <- as.numeric(moda(response[indL])[2])
-    probR <- as.numeric(moda(response[indR])[2])
-    tree[i,"probChildrenSur"] <- (probL*length(indL)/tree[i,"n"]) + (probR*length(indR)/tree[i,"n"])
+  response_var <- names(attr(fit$terms, "dataClasses"))[1]
+  response <- data[[response_var]]
+  
+  # Initialize vimp table
+  vimp_table <- data.frame(variable = character(), 
+                           MeanImpurityDecrease = numeric(), 
+                           stringsAsFactors = FALSE)
+  
+  # Check if classification or regression
+  if (type == "classification") {
+    # Classification logic
+    row.names(tree) <- tree$node
+    tree$prob <- as.numeric(tree$prob)
+    t <- row.names(tree)[tree$terminal==FALSE]
+    tL <- as.character(tree[t,"node"]*2)
+    tR <- as.character(tree[t,"node"]*2+1)
+    tree[t,"probChildren"] <- (tree[tL,"prob"]*tree[tL,"n"]/tree[t,"n"]) + (tree[tR,"prob"]*tree[tR,"n"]/tree[t,"n"])
+    #attach(X)
+    names(response) <- row.names(data)
+    for (i in t){
+      obs <- row.names(data)[unlist(tree[i,"obs"])]
+      path <- tree[i,"splitLabelSur"]
+      x <- data[obs,]
+      indL <- obs[eval(parse(text=paste("x$",path)))]
+      indR <- setdiff(obs,indL)
+      probL <- as.numeric(moda(response[indL])[2])
+      probR <- as.numeric(moda(response[indR])[2])
+      tree[i,"probChildrenSur"] <- (probL*length(indL)/tree[i,"n"]) + (probR*length(indR)/tree[i,"n"])
+    }
+    
+    
+    tree[t,"decProb"] <- tree[t,"probChildren"]-tree[t,"probChildrenSur"]
+    
+    vimp_resp <- tree %>%
+      mutate(Nimp = decImp*n/n[1]) %>%
+      group_by(variable,pred) %>%
+      summarize(vimp = sum(Nimp), .groups = "drop") %>%
+      drop_na(variable) %>%
+      pivot_wider(names_from = pred, values_from = vimp)
+    
+    names(vimp_resp)[-1] <- paste("ImpDec_",names(vimp_resp)[-1])
+    
+    vimp_prob <- tree %>%
+      mutate(Pimp = decProb*n/n[1]) %>%
+      group_by(variable,pred) %>%
+      summarize(pimp = sum(Pimp), .groups = "drop") %>%
+      drop_na(variable) %>%
+      pivot_wider(names_from = pred, values_from = pimp)
+    
+    names(vimp_prob)[-1] <- paste("AccDec_",names(vimp_prob)[-1])
+    
+    vimp <- tree %>%
+      mutate(Nimp = decImp*n/n[1],
+             Pimp = decProb*n/n[1]) %>%
+      group_by(variable) %>%
+      summarize(vimp = sum(Nimp, na.rm=TRUE),
+                pimp = sum(Pimp, na.rm=TRUE), .groups = "drop") %>%
+      drop_na(variable) %>%
+      # mutate(vimpNorm = .data$vimp/sum(.data$vimp)*100,
+      #        pimpNorm = .data$pimp/sum(.data$pimp)*100) %>%
+      arrange(desc(vimp), by_group=FALSE) %>%
+      left_join(vimp_resp, by = "variable") %>%
+      left_join(vimp_prob, by = "variable")
+    
+    names(vimp)[1:3] <- c("Variable","MeanImpurityDecrease","MeanAccuracyDecrease")
+    
+    vimp_table <- vimp_resp
+  } else if (type == "regression") {
+    # Regression logic
+    tree$variance <- as.numeric(tree$prob) # Variance at nodes
+    t <- row.names(tree)[tree$terminal == FALSE]
+    tL <- as.character(tree[t, "node"] * 2)
+    tR <- as.character(tree[t, "node"] * 2 + 1)
+    tree[t, "varianceChildren"] <- (tree[tL, "variance"] * tree[tL, "n"] / tree[t, "n"]) +
+      (tree[tR, "variance"] * tree[tR, "n"] / tree[t, "n"])
+    
+    # Calculate decrease in variance
+    tree[t, "decVariance"] <- tree[t, "variance"] - tree[t, "varianceChildren"]
+    
+    vimp_table <- tree %>%
+      mutate(Nimp = decImp * n / n[1]) %>%
+      group_by(variable) %>%
+      summarize(MeanImpurityDecrease = sum(Nimp, na.rm = TRUE), .groups = "drop") %>%
+      drop_na(variable)
   }
-
-
-  tree[t,"decProb"] <- tree[t,"probChildren"]-tree[t,"probChildrenSur"]
-
-
-  vimp_resp <- tree %>%
-    mutate(Nimp = decImp*n/n[1]) %>%
-    group_by(variable,pred) %>%
-    summarize(vimp = sum(Nimp)) %>%
-    drop_na(variable) %>%
-    pivot_wider(names_from = pred, values_from = vimp)
-
-  names(vimp_resp)[-1] <- paste("ImpDec_",names(vimp_resp)[-1])
-
-  vimp_prob <- tree %>%
-    mutate(Pimp = decProb*n/n[1]) %>%
-    group_by(variable,pred) %>%
-    summarize(pimp = sum(Pimp)) %>%
-    drop_na(variable) %>%
-    pivot_wider(names_from = pred, values_from = pimp)
-
-  names(vimp_prob)[-1] <- paste("AccDec_",names(vimp_prob)[-1])
-
-  vimp <- tree %>%
-    mutate(Nimp = decImp*n/n[1],
-           Pimp = decProb*n/n[1]) %>%
-    group_by(variable) %>%
-    summarize(vimp = sum(Nimp, na.rm=TRUE),
-              pimp = sum(Pimp, na.rm=TRUE)) %>%
-    drop_na(variable) %>%
-    # mutate(vimpNorm = .data$vimp/sum(.data$vimp)*100,
-    #        pimpNorm = .data$pimp/sum(.data$pimp)*100) %>%
-    arrange(desc(vimp), by_group=FALSE) %>%
-    left_join(vimp_resp, by = "variable") %>%
-    left_join(vimp_prob, by = "variable")
-
-  names(vimp)[1:3] <- c("Variable","MeanImpurityDecrease","MeanAccuracyDecrease")
-
-  # Minimal theme + blue fill color
+  
+  # Add ggplot visualizations
+  if (type == "classification") {
+    # Minimal theme + blue fill color
     pImp <-ggplot(vimp, aes(y=Variable, x=MeanImpurityDecrease)) +
       geom_bar(stat="identity", fill="steelblue") +
       scale_y_discrete(limits = rev(vimp$Variable))+
       labs(title="Variable Importance Plot", x = "Mean Impurity Decrease", y = "Variance") +
       theme_minimal()
-
+    
     pAcc <-vimp %>%
       arrange(desc(MeanAccuracyDecrease), by_group=FALSE)
     pAcc <- pAcc %>%
@@ -95,7 +124,21 @@ vimp <- function(fit, data){
       scale_y_discrete(limits = rev(pAcc$Variable))+
       labs(title="Variable Importance Plot", x = "Mean Accuracy Decrease", y = "Variance") +
       theme_minimal()
-
-  res <- list(vimp=vimp, g_imp=pImp, g_acc=pAcc)
-  return(res)
+    
+    res <- list(vimp=vimp, g_imp=pImp, g_acc=pAcc)
+    return(res)
+  } else if (type == "regression"){
+    pImp <- ggplot(vimp_table, aes(y = variable, x = MeanImpurityDecrease)) +
+      geom_bar(stat = "identity", fill = "steelblue") +
+      scale_y_discrete(limits = rev(vimp_table$variable)) +
+      labs(title = "Variable Importance Plot", 
+           x = "Mean Decrease in Impurity", 
+           # Mean Decrease in Impurity is a measure of the importance of a variable in a model based on decision trees (e.g. Random Forest, Decision Trees, Gradient Boosting). 
+           # This value is based on the reduction in impurity that a variable produces when it is used to partition the data into a node of a decision tree. 
+           # For a regression problem, impurity is typically measured through the mean square error (MSE).
+           y = "Variable") +
+      theme_minimal()
+    
+    return(list(vimp = vimp_table, g_imp = pImp))
+  }
 }
