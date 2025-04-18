2a6a10bdcb9ebcbad65bdfe9a92730fe149470e7
Agostino Gnasso <agostinognasso@192.168.1.18>
2025-04-18 12:49:06 +0200

    Minor fix in eComparison

--- R/eComparison.R
+++ R/eComparison.R
@@ -172,12 +172,15 @@ eComparison <- function(data, fit, D, graph = TRUE) {
     
   }
   
-  
-  
+  results <- list(mantel_test = mantel_test, 
+             Proximity_matrix_e2tree = prox_matrix_e2tree,
+             Proximity_matrix_ensemble = prox_matrix_ens)
+
+  print(mantel_test)
   # Return only the Mantel test result and heatmaps
-  return(list(mantel_test = mantel_test, 
-              Proximity_matrix_e2tree = prox_matrix_e2tree,
-              Proximity_matrix_ensemble = prox_matrix_ens))
+  invisible(results)#, 
+              #Proximity_matrix_e2tree = prox_matrix_e2tree,
+              #Proximity_matrix_ensemble = prox_matrix_ens))
 }
 
 
@@ -190,3 +193,4 @@ e2heatmap <- function(data_matrix) {
     col = colorRampPalette(c("white", "black"))(100)
   )
 }
+
